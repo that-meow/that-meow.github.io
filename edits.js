98953fb0c61c52f41e7e78f9e750be3c4b0911a3
that-meow <thatmeow64@gmail.com>
2025-02-27 16:09:40 +0100

    Update edits.js

--- edits.js
+++ edits.js
@@ -7,6 +7,7 @@ async function manager(){
   console.log(stations);
   stations.sort(sortFunctionStations);
   console.log(stations);
+  document.innerHTML("<h1>" + stations[0]["system-name"] + "</h1>);
 }
 
 async function fetchStuff(stuff) {
