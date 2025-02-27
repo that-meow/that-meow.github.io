c30ac9d133b8ceaca37d5d54e4a18813e19964b9
that-meow <thatmeow64@gmail.com>
2025-02-27 15:53:22 +0100

    Update edits.js

--- edits.js
+++ edits.js
@@ -3,12 +3,17 @@ async function manager(){
   console.log(commodities);
   let stations = await fetchStuff("./stations.json");
   console.log(stations);
+  stations.sort(sortFunction);
 }
 
 async function fetchStuff(stuff) {
   return fetch(stuff)
     .then(res => res.json())
     .then(data => {return data});
+}
+
+function sortFunction(a, b) {
+  return a["system-name"] - b["system-name];
 }
 
 manager();
